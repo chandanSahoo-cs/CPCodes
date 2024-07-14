57ef38286de1229b62c1f5ebb2a6f1400558a758
Chandan <141856850+chandanSahoo-cs@users.noreply.github.com>
2024-07-14 16:08:54 +0530

    add cp.cpp

---
+++ templates/templateOne/cp.cpp
@@ -0,0 +1,35 @@
+/*It is not the matter of if, it is the matter of when*/
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#include "chandan/skills.h"
+#define deb(x...)                 \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define deb(x...)
+#endif
+#define ll long long
+#define all(x) begin(x), end(x)
+void solve()
+{
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("onlineJudge/input.txt", "r", stdin);
+    freopen("onlineJudge/error.txt", "w", stderr);
+    freopen("onlineJudge/output.txt", "w", stdout);
+#endif
+    int tt;
+    cin >> tt;
+    while (tt--)
+    {
+        solve();
+    }
+    return 0;
+}
